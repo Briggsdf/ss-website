a02dadfa881e281f4541a568290eb23992a7bfdc
Code World Solutions, LLC <dev-team@codeworld.io>
2021-09-19 16:25:27 -0400

    Update svelte

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,14 @@ import preprocess from 'svelte-preprocess';
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
+import vercel from '@sveltejs/adapter-vercel';
+
+export default {
+	kit: {
+		adapter: vercel()
+	}
+};
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
