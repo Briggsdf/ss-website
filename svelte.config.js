66c21fdc58363385d0be4a0056d9ef99ff0e87df
Code World Solutions, LLC <dev-team@codeworld.io>
2021-09-19 16:27:03 -0400

    Fix it

--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,6 @@ import autoprefixer from 'autoprefixer'
 
 import vercel from '@sveltejs/adapter-vercel';
 
-export default {
-	kit: {
-		adapter: vercel()
-	}
-};
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
@@ -20,11 +14,9 @@ const config = {
         },
         postcss: true,
     }),
-
     kit: {
-        // hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte'
-    }
+		adapter: vercel()
+	}
 };
 
 export default config;
